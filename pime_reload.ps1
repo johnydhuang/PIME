392a6f76500e6820d5e81f5a273eb967678fedc9
Johnny Huang <john.yd.252@gmail.com>
2020-07-11 03:04:11 +0800

    update script

--- pime_reload.ps1
+++ pime_reload.ps1
@@ -1,14 +1,15 @@
-
+$p = Get-Process -Name "PIMELauncher"
+Stop-Process -InputObject $p
 regsvr32 /u "C:\Program Files (X86)\PIME\x86\PIMETextService.dll"
 regsvr32 /u "C:\Program Files (X86)\PIME\x64\PIMETextService.dll"
+Remove-Item -Recurse -Force -Confirm:$false 'C:\Program Files (x86)\PIME\python\input_methods\checorner\'
+Remove-Item -Recurse -Force -Confirm:$false 'C:\Users\Johnny\AppData\Roaming\PIME\checorner'
 cp -r 'D:\Workspace\PIME\python\input_methods\checorner\' 'C:\Program Files (x86)\PIME\python\input_methods\'
-cp -r 'D:\Workspace\PIME\python\input_methods\checj\' 'C:\Program Files (x86)\PIME\python\input_methods\'
 cp 'D:\Workspace\PIME\python\cinbase\json\3corner.json' 'C:\Program Files (x86)\PIME\python\cinbase\json'
 cp 'D:\Workspace\PIME\python\cinbase\__init__.py' 'C:\Program Files (x86)\PIME\python\cinbase\'
 cp 'D:\Workspace\PIME\python\cinbase\configtool.py' 'C:\Program Files (x86)\PIME\python\cinbase\'
 cp 'D:\Workspace\PIME\python\cinbase\debug.py' 'C:\Program Files (x86)\PIME\python\cinbase\'
 cp 'D:\Workspace\PIME\python\cinbase\config\js\config.js' 'C:\Program Files (x86)\PIME\python\cinbase\config\js\'
-Remove-Item 'C:\Program Files (x86)\PIME\python\input_methods\cheez\'
-# cp -r 'D:\Workspace\PIME\python\input_methods\cheez\' 'C:\Program Files (x86)\PIME\python\input_methods\'
 regsvr32 "C:\Program Files (X86)\PIME\x86\PIMETextService.dll"
 regsvr32 "C:\Program Files (X86)\PIME\x64\PIMETextService.dll"
+& 'C:\Program Files (x86)\PIME\PIMELauncher.exe'
